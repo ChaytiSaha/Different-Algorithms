45cb503dbbb9a3e4a96f63a664fbfc80ab3f464a
Chayti Saha <chaytisaha02121998@gmail.com>
2018-08-19 12:12:12 +0600

    Add files via upload

---
+++ base conversion.cpp
@@ -0,0 +1,105 @@
+/**
+Sample input
+-----------
+3
+101 bin
+101 dec
+8f hex
+
+Sample output
+------------
+Case 1:
+5 dec
+5 hex
+
+Case 2:
+65 hex
+1100101 bin
+
+Case 3:
+143 dec
+10001111 bin
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+char digit_hex(int x)
+{
+    if(x >= 0 && x < 10) return (x + '0');
+    else if (x < 16) return (x - 10 + 'a');
+    else return '!';
+}
+
+string tobin(int x)
+{
+    string tmp;
+    char c;
+    while(x > 0)
+    {
+        c = x % 2 + '0';
+        tmp = c + tmp;
+        x /= 2;
+    }
+    return tmp;
+}
+
+string tohex(int x)
+{
+    string tmp;
+    while(x > 0)
+    {
+        tmp = digit_hex(x % 16) + tmp;
+        x /= 16;
+    }
+    return tmp;
+}
+
+void bin(char * c, string s)
+{
+    int x = strtol(c, 0, 2);
+    cout << x << " dec" << endl;
+    cout << tohex(x) << " hex" << endl;
+}
+
+void dec(char * c, string s)
+{
+    int x = strtol(c, 0, 10);
+    cout << tohex(x) << " hex" << endl;
+    cout << tobin(x) << " bin" << endl;
+}
+
+void hex(char * c, string s)
+{
+    int x = strtol(c, 0, 16);
+    cout << x << " dec" << endl;
+    cout << tobin(x) << " bin" << endl;
+}
+
+int main()
+{
+    int n, count = 1;
+    char c[50];
+    string s;
+
+    cin >> n;
+
+    while(n--)
+    {
+        cin >> c >> s;
+        cout << "Case " << count << ":" << endl;
+
+        if(s == "bin"){
+            bin(c, s);
+        }else if(s == "dec"){
+            dec(c, s);
+        }else{
+            hex(c, s);
+        }
+
+        count++;
+        cout << endl;
+    }
+    return 0;
+}
+
